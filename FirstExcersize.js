89dc1ddd186e0e4c224eec50a2835ade5827a524
madhuraps <mapsenevi@gmail.com>
2020-09-02 19:40:54 +1000

    Add Values

---
+++ FirstExcersize.js
@@ -0,0 +1,12 @@
+
+function Main() {
+    let x=10; 
+    let y=15;
+    console.log(`The Value of ${y} + ${x} = ${(x+y)}`);
+    console.log(`The Value of ${y} - ${x} = ${(x-y)}`);
+    console.log(`The Value of ${y} * ${x} = ${(x*y)}`);
+    console.log(`The Value of ${y} / ${x} = ${(x/y)}`);
+
+
+}
+Main();
