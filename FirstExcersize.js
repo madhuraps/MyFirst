2f1a4b36528eb269cbe7dc0a848c3003a550950b
madhuraps <mapsenevi@gmail.com>
2020-09-02 19:59:30 +1000

    New Functions Added

--- FirstExcersize.js
+++ FirstExcersize.js
@@ -1,12 +1,33 @@
-
 function Main() {
-    let x=10; 
-    let y=15;
-    console.log(`The Value of ${y} + ${x} = ${(x+y)}`);
-    console.log(`The Value of ${y} - ${x} = ${(x-y)}`);
-    console.log(`The Value of ${y} * ${x} = ${(x*y)}`);
-    console.log(`The Value of ${y} / ${x} = ${(x/y)}`);
+    let x = 10;
+    let y = 15;
+
+    function add() {
+
+        console.log(`The Value of ${y} + ${x} = ${(x+y)}`);
+    }
+
+    function substract() {
+        console.log(`The Value of ${y} - ${x} = ${(x-y)}`);
+
+    }
+
+    function multiply() {
+        console.log(`The Value of ${y} * ${x} = ${(x*y)}`);
+
+    }
+
+    function divide() {
+        console.log(`The Value of ${y} / ${x} = ${(x/y)}`);
+
+    }
+
+    add();
+    substract();
+    multiply();
+    divide();
 
 
 }
+
 Main();
